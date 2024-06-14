ab1bf6e8151cc90351851b978e444c1a2d7301c2
omshelke0508 <164185644+omshelke0508@users.noreply.github.com>
2024-06-14 21:43:01 +0530

    Add files via upload

---
+++ swiperjs/script.js
@@ -0,0 +1,61 @@
+var tl = gsap.timeline();
+
+tl.to(".nav-right h6", {
+    x: 20,
+    opaCity: 0,
+    duration: .3
+})
+tl.to(".nav-right h1", {
+    x: 20,
+    opaCity: 0,
+    duration: .3
+})
+
+
+tl.to(".nav-left h4", {
+    x: 30,
+    opaCity: 0,
+    duration: .2
+})
+tl.to(".nav-left ", {
+    x: 30,
+    opaCity: 0,
+    duration: .2
+})
+tl.to(".home2 h1", {
+    x: 40,
+    opaCity: 0,
+    duration: .2
+})
+
+tl.to(".home2 imga", {
+    x: 40,
+    opaCity: 0,
+    duration: .2
+})
+
+tl.to(".home1 img", {
+    y: 25,
+    opaCity: 0,
+    duration: .2
+})
+tl.to(".home1 h1", {
+    y: 25,
+    opaCity: 0,
+    duration: .2
+})
+tl.to(".home1 h3", {
+    y: 25,
+    opaCity: 0,
+    duration: .2
+})
+tl.to(".home1 h5", {
+    y: 25,
+    opaCity: 0,
+    duration: .2
+})
+tl.to(".home1 button", {
+    y: 25,
+    opaCity: 0,
+    duration: .2
+})
